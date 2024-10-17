dec1c0de86cdb5b63447159c88fb4926c79d62a3
Arturrr13 <98638523+Arturrr13@users.noreply.github.com>
2024-10-17 14:50:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
 app.use('/api', index_1.default);
 app.listen(PORT, () => {
-    console.log('YEP');
+    console.log('YEP', PORT);
 });
